aaf19c777d6ec29afa2ffa73d6256b4539680d76
= <dco3@cin.ufpe.br>
2022-03-10 21:25:11 -0300

    fix: solve challenge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <CharacterEditor />
-      <Footer />
+      <div className="desktop">
+        <Footer />
+      </div>
     </>
   );
 }
